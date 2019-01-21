eb3997a903c51d2eb6bf1edad6f8f23b1fa96bb1
lampitosgames <dantimko1@gmail.com>
2019-01-21 17:27:58 -0500

    Working update to ECS

--- ggp-engine/FamilyTypeID.cpp
+++ ggp-engine/FamilyTypeID.cpp
@@ -1,10 +1,8 @@
 #include "stdafx.h"
 #include "FamilyTypeID.h"
 
-
 namespace ECS {
 	class IComponent;
-
 	namespace Util {
 		TypeID FamilyTypeID<IComponent>::s_count = 0u;
 
--- ggp-engine/IComponent.h
+++ ggp-engine/IComponent.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef GGP_ICOMPONENT_H
+#define GGP_ICOMPONENT_H
 
 #include <string>
 
@@ -7,47 +8,33 @@ namespace ECS {
 	using ComponentID = size_t;
 	using ComponentTypeId = size_t;
 	using EntityID = std::string;
-
 	class GameObject;
 
-	/// <summary>
-	/// Abstract component class with basic functionality that every 
-	/// component should have.
-	/// </summary>
+	//Abstract component class with basic functionality that every 
+	//component should have.
 	class IComponent {
 		friend class ComponentManager;
 
 	public:
-
 		IComponent() {}
-
 		virtual ~IComponent() {}
 
 		inline const bool operator==(const IComponent& other) const { return id == other.id; }
 		inline const bool operator!=(const IComponent& other) const { return id == other.id; }
 
-		/////////////////////////////////////////////////    
-		// Accessors 
-
 		inline const ComponentID GetComponentId() const { return this->id; }
-
 		inline const bool IsEnabled() const { return this->isEnabled; }
-
 		inline void SetSenabled(bool aEnabledState) { this->isEnabled = aEnabledState; }
-
 		const EntityID& GetOwner() const { return this->owner; }
-
 	protected:
-
-		/** The unique ID of this component */
+		//The unique ID of this component
 		ComponentID id = -1;
 
-		/** If this component is enabled or not */
+		//If this component is enabled or not
 		bool isEnabled = true;
 
-		/** The owner of this component */
+		//The owner of this component
 		EntityID owner;
-
-	};  // IComponent
-
-}   // namespace ECS
+	};
+}
+#endif //GGP_ICOMPONENT_H
